700d91a19f1eee00266725bcb108c77ab6d3732d
guakia12 <guakia12@gmail.com>
2014-10-18 05:34:43 +0900

    imgs are moving

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var spreadSheet, mergedCellsSheet, sheetEdge;
-var _cell, _xEdgeCell, _yEdgeCell, _sheetUnit, _mergedCell, cellNum;
+var mergedCellsSheet;
+var _cell, _xEdgeCell, _yEdgeCell, _sheetUnit, _mergedCell; 
+var xCellNum, yCellNum, totaltotalCellNum;
 var pastCurPosX, pastCurPosY; 
 var contents = [];
 var isMouseDown = false;
@@ -8,31 +9,29 @@ var target;
 var init = function(){
 	document.body.style.height = window.innerHeight + 200;
 	document.body.style.width = window.innerWidth + 200;
+	setMoving();
 
-	spreadSheet = document.getElementById('spread_sheet');
-	sheetEdge = document.getElementById('sheet_edge');
-	mergedCellsSheet = document.getElementById('merged_cells_sheet');
 	_cell = {type: 'div', className : 'cell', length : 20};
 	_xEdgeCell = {type: 'div', className : 'x_edge_cell'};
 	_yEdgeCell = {type: 'div', className : 'y_edge_cell'};
 	_sheetUnit = {type : 'div', className : 'sheet_unit', length : 500};
 	_mergedCell = {type: 'div', className :'merged_cell', length : 60};
-	cellNum = ((window.innerHeight+200)/_cell.length)*((window.innerWidth+200)/_cell.length);
-	createSheet(_cell, cellNum);
+
+	xCellNum = Math.floor((window.innerWidth+200)/_cell.length);
+	yCellNum = Math.floor((window.innerHeight+200)/_cell.length);
+	totalCellNum = (xCellNum*yCellNum);
+	createSheet(_cell, totalCellNum);
 	createEdge(_xEdgeCell, _yEdgeCell);
-	createMergedCell(_sheetUnit, cellNum);
+	createMergedCell(_sheetUnit, totalCellNum);
 
-	contents.push(document.getElementById('information'));
-	contents.push(document.getElementById('countdown'));
-	contents.push(document.getElementById('introduction'));
+	countDownTimer('10/31/2014 09:00 AM', 'countdown');
 	setMouseEvent(contents);
-
 	window.onmousemove = checkRefresh;
 	window.onmouseup = function(){ put(); isMouseDown = false; window.onmousemove = checkRefresh; };
-	countDownTimer('10/31/2014 09:00 AM', 'countdown');
 };
 
 var createSheet = function(obj, num){
+	var spreadSheet = document.getElementById('spread_sheet');
 	var i = 0;
 	while( i<num ){
 		var cell = document.createElement(obj.type);
@@ -45,37 +44,32 @@ var createSheet = function(obj, num){
 };
 
 var createEdge = function(objX, objY){
-	var i = 0;
+	var sheetEdge = document.getElementById('sheet_edge');
 	var j = 0;
 	var k = 0;
-	while( i<Math.floor((window.innerWidth+200)/_cell.length)){
+	for( var i=0 ; i<xCellNum ; i++ ){
 		var xEdgeCell = document.createElement(objX.type);
 		xEdgeCell.className = objX.className;
 		if(i>1){	
 			if(i>=28){
 				xEdgeCell.innerHTML = String.fromCharCode(97+j)+String.fromCharCode(97+k);
 				if( k<25 ){ k++; }else{ j++; k=0; };
-			}else{
-				xEdgeCell.innerHTML = String.fromCharCode(97+i-2);
-			}
+			}else{ xEdgeCell.innerHTML = String.fromCharCode(97+i-2); }
 		}
 		sheetEdge.appendChild(xEdgeCell);
-		i++;
 	}
-	i = 0;
-	while( i<Math.floor((window.innerHeight+200)/_cell.length)){
+
+	for( var i=0 ; i<yCellNum ; i++ ){
 		var yEdgeCell = document.createElement(objY.type);
 		yEdgeCell.className = objY.className;
 		yEdgeCell.style.top = 20*i;
-		if(i>0){
-			yEdgeCell.innerHTML = i;
-		}
+		if(i>0){ yEdgeCell.innerHTML = i; }
 		sheetEdge.appendChild(yEdgeCell);
-		i++;
 	}
 };
 
 var createMergedCell = function(obj, num){
+	var mergedCellsSheet = document.getElementById('merged_cells_sheet');
 	while(mergedCellsSheet.hasChildNodes()){ mergedCellsSheet.removeChild(mergedCellsSheet.firstChild); }	
 	var i = 0;
 	while( i<num/600 ){
@@ -105,7 +99,7 @@ var setMergedCell = function(obj){
 var checkRefresh = function(e){
 	var distanceX = Math.abs(e.pageX-pastCurPosX);
 	var distanceY = Math.abs(e.pageY-pastCurPosY);
-	if(distanceX>50 && distanceY>50){ createMergedCell(_sheetUnit, cellNum); };
+	if(distanceX>50 && distanceY>50){ createMergedCell(_sheetUnit, totalCellNum); };
 	pastCurPosX = e.pageX;
 	pastCurPosY = e.pageY;
 };
@@ -130,7 +124,41 @@ var countDownTimer = function(dt, id){
 	timer = setInterval(showRemaining, 1000);
 };
 
+var setMoving = function(){
+	var slowMovingImgs = document.getElementById('moving_images_slow');
+	var mediumMovingImgs = document.getElementById('moving_images_medium');
+	var fastMovingImgs = document.getElementById('moving_images_fast');
+	var setSpeed = function(){
+		for( i=0 ; i<slowMovingImgs.childNodes.length ; i++ ){
+			var tmp = slowMovingImgs.childNodes[i];
+			var x = (Math.floor(Math.random()*13)*40);
+			var y = (Math.floor(Math.random()*13)*40);
+			tmp.style.left = Math.random() < 0.5 ? tmp.offsetLeft-x : tmp.offsetLeft+x ;
+			tmp.style.top = Math.random() < 0.5 ? tmp.offsetTop-y : tmp.offsetTop+y ;
+		}
+		for( i=0 ; i<mediumMovingImgs.childNodes.length ; i++ ){
+			var tmp = mediumMovingImgs.childNodes[i];
+			var x = (Math.floor(Math.random()*13)*80);
+			var y = (Math.floor(Math.random()*13)*80);
+			tmp.style.left = Math.random() < 0.5 ? tmp.offsetLeft-x : tmp.offsetLeft+x ;
+			tmp.style.top = Math.random() < 0.5 ? tmp.offsetTop-y : tmp.offsetTop+y ;
+		}
+		for( i=0 ; i<fastMovingImgs.childNodes.length ; i++ ){
+			var tmp = fastMovingImgs.childNodes[i];
+			var x = (Math.floor(Math.random()*10)*120);
+			var y = (Math.floor(Math.random()*10)*120);
+			tmp.style.left = Math.random() < 0.5 ? tmp.offsetLeft-x : tmp.offsetLeft+x ;
+			tmp.style.top = Math.random() < 0.5 ? tmp.offsetTop-y : tmp.offsetTop+y ;
+		}	
+	};
+	setSpeed();
+	setInterval(setSpeed, 5000);
+};
+
 var setMouseEvent = function(arr){
+	arr.push(document.getElementById('information1'));
+	arr.push(document.getElementById('countdown'));
+	arr.push(document.getElementById('introduction'));
 	for(var i=0 ; i<arr.length ; i++){
 		arr[i].onmousedown = function(e){ isMouseDown = true; pick(e, this); };
 	}
